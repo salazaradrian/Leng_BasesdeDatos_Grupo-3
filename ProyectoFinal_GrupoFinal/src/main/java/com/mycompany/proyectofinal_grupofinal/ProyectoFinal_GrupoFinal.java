df028af566c63f2f63e2227aed094b1d2cfc6222
salazaradrian <87552347+salazaradrian@users.noreply.github.com>
2025-06-30 18:53:54 -0600

    Editar

---
+++ ProyectoFinal_GrupoFinal/src/main/java/com/mycompany/proyectofinal_grupofinal/ProyectoFinal_GrupoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal_grupofinal;
+
+/**
+ *
+ * @author Adrian Salazar R
+ */
+public class ProyectoFinal_GrupoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
