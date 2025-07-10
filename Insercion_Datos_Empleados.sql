b95dd35691e9503104dc59821f83255c73a1dbe9
Jeff <159392804+TheJeff182@users.noreply.github.com>
2025-07-09 19:24:03 -0600

    Update Insercion_Datos_Empleados.sql

---
+++ Insercion_Datos_Empleados.sql
@@ -0,0 +1,19 @@
+--Gerente
+INSERT INTO empleados (nombre, primer_apellido, segundo_apellido, salario, cargo)
+VALUES ('Roberto', 'Fuentes', 'Montero', 1150000.00, 'Gerente');
+
+-- Panadero principal
+INSERT INTO empleados (nombre, primer_apellido, segundo_apellido, salario, cargo)
+VALUES ('Luis', 'Castro', 'Vargas', 850000.00, 'Panadero principal');
+
+-- Pastelero
+INSERT INTO empleados (nombre, primer_apellido, segundo_apellido, salario, cargo)
+VALUES ('Adriana', 'Mendoza', 'Herrera', 880000.00, 'Pastelero');
+
+-- Ayudante de panadería/pastelería
+INSERT INTO empleados (nombre, primer_apellido, segundo_apellido, salario, cargo)
+VALUES ('Esteban', 'Brenes', 'Ruiz', 520000.00, 'Ayudante de panadería/pastelería');
+
+-- Vendedor
+INSERT INTO empleados (nombre, primer_apellido, segundo_apellido, salario, cargo)
+VALUES ('Maria', 'Gutierrez', 'Marin', 450000.00, 'Vendedor');
