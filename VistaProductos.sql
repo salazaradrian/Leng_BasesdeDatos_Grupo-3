77f25a4db91115f3563c098c36e04f8968f3fcbf
Francv19 <160301835+Francv19@users.noreply.github.com>
2025-07-14 20:18:33 -0700

    Update VistaProductos.sql

--- VistaProductos.sql
+++ VistaProductos.sql
@@ -1,4 +1,12 @@
-CREATE OR REPLACE VIEW Vista_de_Productos_caros AS
-SELECT nombre,tipo,precio
-FROM productos
-WHERE precio >= 2000
+CREATE OR REPLACE VIEW Vista_de_Ventas AS
+SELECT    v.id_ventas,
+    c.nombre || ' ' || c.primer_apellido AS cliente,
+    e.nombre || ' ' || e.primer_apellido AS empleado,
+    p.nombre AS producto,
+    p.precio,
+    v.cantidad_productos_total,
+    v.monto_total
+FROM ventas v
+INNER JOIN clientes c ON v.id_cliente = c.id_cliente
+INNER JOIN empleados e ON v.id_empleado = e.id_empleado
+INNER JOIN productos p ON v.id_producto = p.id_producto;
