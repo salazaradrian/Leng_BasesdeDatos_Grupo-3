598f0c52afdae69253fe112a485f8d11104bc0ab
Jeff <159392804+TheJeff182@users.noreply.github.com>
2025-08-07 20:44:22 -0600

    Update Factura.java
    Removiendo campo 'estado' con sus getters y setters ya que no se necesitan

--- ProyectoFinal_GrupoFinal/src/main/java/modelo/Factura.java
+++ ProyectoFinal_GrupoFinal/src/main/java/modelo/Factura.java
@@ -2,25 +2,28 @@ package modelo;
 
 import java.util.Date;
 
+
 public class Factura {
+
     private int idFactura;
     private int idVentas;
     private Date fecha;
     private double impuesto;
     private double subtotal;
-    private int idEstado;
 
-    public Factura() {}
-
-    public Factura(int idFactura, int idVentas, Date fecha, double impuesto, double subtotal, int idEstado) {
+    
+    public Factura() {
+    }
+   
+    public Factura(int idFactura, int idVentas, Date fecha, double impuesto, double subtotal) {
         this.idFactura = idFactura;
         this.idVentas = idVentas;
         this.fecha = fecha;
         this.impuesto = impuesto;
         this.subtotal = subtotal;
-        this.idEstado = idEstado;
     }
 
+    // Getters y Setters
     public int getIdFactura() {
         return idFactura;
     }
@@ -61,11 +64,88 @@ public class Factura {
         this.subtotal = subtotal;
     }
 
-    public int getIdEstado() {
-        return idEstado;
-    }
-
-    public void setIdEstado(int idEstado) {
-        this.idEstado = idEstado;
+    @Override
+    public String toString() {
+        return "Factura{" +
+               "idFactura=" + idFactura +
+               ", idVentas=" + idVentas +
+               ", fecha=" + fecha +
+               ", impuesto=" + impuesto +
+               ", subtotal=" + subtotal +
+               '}';
     }
 }
+
+
+
+//package modelo;
+//
+//import java.util.Date;
+//
+//public class Factura {
+//    private int idFactura;
+//    private int idVentas;
+//    private Date fecha;
+//    private double impuesto;
+//    private double subtotal;
+//    private int idEstado;
+//
+//    public Factura() {}
+//
+//    public Factura(int idFactura, int idVentas, Date fecha, double impuesto, double subtotal, int idEstado) {
+//        this.idFactura = idFactura;
+//        this.idVentas = idVentas;
+//        this.fecha = fecha;
+//        this.impuesto = impuesto;
+//        this.subtotal = subtotal;
+//        this.idEstado = idEstado;
+//    }
+//
+//    public int getIdFactura() {
+//        return idFactura;
+//    }
+//
+//    public void setIdFactura(int idFactura) {
+//        this.idFactura = idFactura;
+//    }
+//
+//    public int getIdVentas() {
+//        return idVentas;
+//    }
+//
+//    public void setIdVentas(int idVentas) {
+//        this.idVentas = idVentas;
+//    }
+//
+//    public Date getFecha() {
+//        return fecha;
+//    }
+//
+//    public void setFecha(Date fecha) {
+//        this.fecha = fecha;
+//    }
+//
+//    public double getImpuesto() {
+//        return impuesto;
+//    }
+//
+//    public void setImpuesto(double impuesto) {
+//        this.impuesto = impuesto;
+//    }
+//
+//    public double getSubtotal() {
+//        return subtotal;
+//    }
+//
+//    public void setSubtotal(double subtotal) {
+//        this.subtotal = subtotal;
+//    }
+//
+//    public int getIdEstado() {
+//        return idEstado;
+//    }
+//
+//    public void setIdEstado(int idEstado) {
+//        this.idEstado = idEstado;
+//    }
+//}
