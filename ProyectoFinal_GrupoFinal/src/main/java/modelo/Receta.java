c8201257987babb1d56014f732e701f191b0023f
salazaradrian <87552347+salazaradrian@users.noreply.github.com>
2025-08-10 16:16:41 -0600

    Corrección de Error -  Tabla Recetas

--- ProyectoFinal_GrupoFinal/src/main/java/modelo/Receta.java
+++ ProyectoFinal_GrupoFinal/src/main/java/modelo/Receta.java
@@ -1,19 +1,70 @@
+//package modelo;
+//
+//public class Receta {
+//    private int idReceta;
+//    private String nombre;
+//    private int idIngrediente;
+//
+//    // Constructor vacío
+//    public Receta() {
+//    }
+//
+//    // Constructor con parámetros
+//    public Receta(int idReceta, String nombre, int idIngrediente) {
+//        this.idReceta = idReceta;
+//        this.nombre = nombre;
+//        this.idIngrediente = idIngrediente;
+//    }
+//
+//    // Getters y Setters
+//    public int getIdReceta() {
+//        return idReceta;
+//    }
+//
+//    public void setIdReceta(int idReceta) {
+//        this.idReceta = idReceta;
+//    }
+//
+//    public String getNombre() {
+//        return nombre;
+//    }
+//
+//    public void setNombre(String nombre) {
+//        this.nombre = nombre;
+//    }
+//
+//    public int getIdIngrediente() {
+//        return idIngrediente;
+//    }
+//
+//    public void setIdIngrediente(int idIngrediente) {
+//        this.idIngrediente = idIngrediente;
+//    }
+//
+//    // Para mostrar en consola o debug
+//    @Override
+//    public String toString() {
+//        return "Receta{" +
+//                "idReceta=" + idReceta +
+//                ", nombre='" + nombre + '\'' +
+//                ", idIngrediente=" + idIngrediente +
+//                '}';
+//    }
+//}
 package modelo;
 
 public class Receta {
     private int idReceta;
     private String nombre;
-    private int idIngrediente;
 
     // Constructor vacío
     public Receta() {
     }
 
     // Constructor con parámetros
-    public Receta(int idReceta, String nombre, int idIngrediente) {
+    public Receta(int idReceta, String nombre) {
         this.idReceta = idReceta;
         this.nombre = nombre;
-        this.idIngrediente = idIngrediente;
     }
 
     // Getters y Setters
@@ -33,21 +84,12 @@ public class Receta {
         this.nombre = nombre;
     }
 
-    public int getIdIngrediente() {
-        return idIngrediente;
-    }
-
-    public void setIdIngrediente(int idIngrediente) {
-        this.idIngrediente = idIngrediente;
-    }
-
     // Para mostrar en consola o debug
     @Override
     public String toString() {
         return "Receta{" +
                 "idReceta=" + idReceta +
                 ", nombre='" + nombre + '\'' +
-                ", idIngrediente=" + idIngrediente +
                 '}';
     }
 }
